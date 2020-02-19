e29962177f3811efe315cc46f96aba4f6d2b0df0
jaimesaez97 <jsaezdeb@ucm.es>
2020-02-19 16:58:59 +0100

    Changed to several inputs

--- Entrega/implementacion_jlex/Main.java
+++ Entrega/implementacion_jlex/Main.java
@@ -8,19 +8,31 @@ import java.io.Reader;
 
 public class Main {
 
+    private final int NUM_FILES = 3;
+    
 	public static void main(String[] args){
 
 		Reader input;
+        int i = 1;
+        String inputFile;
 
 		try{
-			input = new InputStreamReader(new FileInputStream("input.txt"));
-			TinyLexicalAnalyzer la;
-			la = new TinyLexicalAnalyzer(input);
-			LexicalUnit unit;
-			do{
-				unit = la.yylex();
-				System.out.println(unit);
-			} while(unit.lexClass() != LexicalClass.EOF);
+            while(i <= NUM_FILES){
+                inputFile = "input";
+                inputFile.concat(i);
+                inputFile.concat(".txt");
+                
+                input = new InputStreamReader(new FileInputStream(inputFile));
+                TinyLexicalAnalyzer la;
+                la = new TinyLexicalAnalyzer(input);
+                LexicalUnit unit;
+                do{
+                    unit = la.yylex();
+                    System.out.println(unit);
+                } while(unit.lexClass() != LexicalClass.EOF);
+                
+                i += 1;
+            }
 				
 		} catch(FileNotFoundException e){
 			e.printStackTrace();
--- Entrega/implementacion_manual/Main.java
+++ Entrega/implementacion_manual/Main.java
@@ -6,19 +6,32 @@ import java.io.Reader;
 
 public class Main {
 
+    private final int NUM_FILES = 3;
+    
 	public static void main(String[] args){
 
 		Reader input;
-
+        int i = 1;
+        String inputFile;
+        
 		try{
-			input = new InputStreamReader(new FileInputStream("input.txt"));
-			LexicalAnalyzer la;
-			la = new LexicalAnalyzer(input);
-			LexicalUnit unit;
-			do{
-				unit = la.nextToken();
-				System.out.println(unit);
-			} while(unit.lexClass() != LexicalClass.EOF);
+            while(i <= NUMFILES){
+                inputFile = "input";
+                inputFile.concat(i);
+                inputFile.concat(".txt");
+            
+                input = new InputStreamReader(new FileInputStream(inputFile));
+                
+                LexicalAnalyzer la;
+                la = new LexicalAnalyzer(input);
+                LexicalUnit unit;
+                do{
+                    unit = la.nextToken();
+                    System.out.println(unit);
+                } while(unit.lexClass() != LexicalClass.EOF);
+                
+                i += 1;
+            }
 				
 		} catch(FileNotFoundException e){
 			e.printStackTrace();
